424a7b056cf9bfced2fcfaea2d736d40415d6d0c
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-06-15 17:57:24 +0400

    საბოლოო ქომითი

--- app/src/main/java/com/example/firstassignment/mish.kt
+++ app/src/main/java/com/example/firstassignment/mish.kt
@@ -1,21 +1,19 @@
 package com.example.firstassignment
 
-import java.lang.NumberFormatException
-import java.util.*
-
-
-
 fun main(){
+
     calculate()
 }
 
 fun calculate(){
 
     println("შეიყვანეთ პრიველი რიცხვი")
-    var X = readLine()!!.toInt()
+    val X = readLine()!!.toInt()
+
     println("შეიყვანეთ მეორე რიცხვი")
-    var Y = readLine()!!.toInt()
-    var Z = X + Y
+    val Y = readLine()!!.toInt()
+
+    val Z = X + Y
     println("$X-ის და $Y-ს ჯამი არის $Z")
 
     continueOrNot()
@@ -25,14 +23,10 @@ fun calculate(){
 fun continueOrNot(){
     println("გსურთ თამაშის გაგრძელება? (ჩაწერეთ Y ან N)")
 
-    var answer = readLine()
-
-    if(answer == "N"){
-        println("ნახვამდის!")
-    }else if (answer == "Y"){
-        calculate()
-    }else{
-        println("ჩაწერეთ მხოლოდ N ან Y")
+    when (readLine()) {
+        "N" -> {println("ნახვამდის!")}
+        "Y" -> {calculate()}
+        else -> {println("ჩაწერეთ მხოლოდ N ან Y")}
     }
 }
 
